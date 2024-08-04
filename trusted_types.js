5b84e392d0fe819c808344620e3b70c1cea30a3b
stephenhawk8054 <66517106+stephenhawk8054@users.noreply.github.com>
2024-08-04 08:49:13 +0700

    fixed in uBO

--- trusted_types.js
+++ trusted_types.js
@@ -1,12 +1,1 @@
-'use strict';
 
-/// set-trusted-types.js
-/// dependency run-at.fn
-function setTrustedTypes() {
-  if (window.trustedTypes && window.trustedTypes.createPolicy) {
-    window.trustedTypes.createPolicy("default", {
-      createScript: string => string
-    });
-  }
-  runAt(() => {}, 'interactive');
-}
